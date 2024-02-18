55e8acf314ef53e2f3c76234dd65724eb5540993
juanludell <juanluis.belda@gmail.com>
2024-02-18 19:17:17 +0100

    ci: prepare playwright

---
+++ playwright.config.ts
@@ -0,0 +1,40 @@
+import {type PlaywrightTestConfig, devices } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+    globalSetup: "",
+    testDir: 'test/',
+    reporter: 'html',
+    workers: 5,
+    retries: 1,
+    use: {
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'retain-on-failure',
+        baseURL: process.env.BASE_URL || 'http://localhost:8080',
+        storageState: 'test/storage-state.json'
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { browserName: 'chromium' }
+        },
+        {
+            name: 'firefox',
+            use: { browserName: 'firefox' }
+        },
+        // {
+        //     name: 'webKit',
+        //     use: { browserName: 'webkit' }
+        // },
+        {
+            name: 'mobile-chromium',
+            use: devices['Pixel 5']
+        },
+        // {
+        //     name: 'mobile-webkit',
+        //     use: devices['iPhone 13']
+        // }
+    ]
+}
+
+export default config
